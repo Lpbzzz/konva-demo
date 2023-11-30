c22e7dad1ea5b73a9b40887d4873cae9caafceee
liupengbo <liupengbo@vv.cn>
2023-11-30 13:42:24 +0800

    add canvas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import {Layer, Rect, Stage} from "react-konva";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{width:'500px',height:'500px',border:'1px solid #ccc'}}>
+        <Stage width={window.innerWidth} height={window.innerHeight}>
+            <Layer>
+                <Rect x={20} y={20} width={100} height={100} fill="red" shadowBlur={10} />
+            </Layer>
+        </Stage>
     </div>
   );
 }
